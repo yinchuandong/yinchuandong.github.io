a6e5af3b999786c5a9f072b1962694bfbc8fe9fb
yinchuandong <johncdyin@gmail.com>
2018-08-07 08:55:36 +1000

    update:

--- scp.sh
+++ scp.sh
@@ -2,9 +2,9 @@ dist="dist"
 static="dist/static"
 
 files="$dist/index.html"
-#scp -r $files ubuntu@www.yinchuandong.com:/var/www/html
-scp -r $files ubuntu@www.yinchuandong.com:/var/www/www.yinchuandong.com
+scp -r $files ubuntu@www.yinchuandong.com:/var/www/html
+#scp -r $files ubuntu@www.yinchuandong.com:/var/www/www.yinchuandong.com
 
 files="$static/css $static/img $static/js $static/particles.json $static/pa4c.pdf"
-#scp -r $files ubuntu@www.yinchuandong.com:/var/www/html/static
-scp -r $files ubuntu@www.yinchuandong.com:/var/www/www.yinchuandong.com/static
+scp -r $files ubuntu@www.yinchuandong.com:/var/www/html/static
+#scp -r $files ubuntu@www.yinchuandong.com:/var/www/www.yinchuandong.com/static
